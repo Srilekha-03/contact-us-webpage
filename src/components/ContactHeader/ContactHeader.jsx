bb5d167cf81d23afb30527ef042cb2f071c471b3
Srilekha-03 <srilekhakasha47@gmail.com>
2024-07-11 23:45:25 +0530

    hmm failed in matching my design with the original design, could not figure what is wrong with the the contactHeader

--- src/components/ContactHeader/ContactHeader.jsx
+++ src/components/ContactHeader/ContactHeader.jsx
@@ -1,16 +1,10 @@
 import styles from "./ContactHeader.module.css";
 const ContactHeader = () => {
   return (
-    <div className={`${styles.contact_section}`}>
-      
+    <div className={`container ${styles.contact_section}`}>
+      <h1>CONTACT US</h1>
+      <p>LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA</p>
 
-      <h6>CONTACT US</h6>
-      <p>
-        LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU!
-        WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN
-        REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE,
-        EMAIL, OR SOCIAL MEDIA.{" "}
-      </p>
     </div>
   );
 };
